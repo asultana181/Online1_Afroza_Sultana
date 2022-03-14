6ba13939b228ab96596dafa7a9d65868dbe83857
Sathy <asultana181085@bscse.uiu.ac.bd>
2022-03-14 14:41:58 +0600

    index added

---
+++ index.php
@@ -0,0 +1,11 @@
+<h1>Airplane Management System</h1>
+
+
+
+Database: airplane_management<br>
+
+Table: ticket_booking<br><br>
+
+<a href=addBooking.php>AddBooking</a> <br>
+<a href=cancelBooking.php>CancelBooking</a>
+
